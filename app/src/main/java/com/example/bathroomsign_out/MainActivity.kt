945e95e19dacde09b6d682e9e58cf331ee609779
Mitchell Beaudoin <mitchellbeaudoin101@gmail.com>
2021-11-21 02:12:58 -0500

    Version 1.7

--- app/src/main/java/com/example/bathroomsign_out/MainActivity.kt
+++ app/src/main/java/com/example/bathroomsign_out/MainActivity.kt
@@ -25,17 +25,56 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 var name = spinner.selectedItemPosition
+                val teachers = resources.getStringArray(R.array.brew_array).toList()
                 when (name) {
-                    0 -> selectedname = "Scott"
-                    1 -> selectedname = "Tierney"
-                    2 -> selectedname = "Avcollie"
-                    3 -> selectedname = "Johnson"
-                    4 -> selectedname = "Fuller"
-                    5 -> selectedname = "Erikson"
-                    6 -> selectedname = "Maslak"
-                    7 -> selectedname = "Foulds"
-                    8 -> selectedname = "Miller"
-                    9 -> selectedname = "Sconz"
+                    0 -> selectedname = teachers[0]
+                    1 -> selectedname = teachers[1]
+                    2 -> selectedname = teachers[2]
+                    3 -> selectedname = teachers[3]
+                    4 -> selectedname = teachers[4]
+                    5 -> selectedname = teachers[5]
+                    6 -> selectedname = teachers[6]
+                    7 -> selectedname = teachers[7]
+                    8 -> selectedname = teachers[8]
+                    9 -> selectedname = teachers[9]
+                    10 -> selectedname = teachers[10]
+                    11 -> selectedname = teachers[11]
+                    12 -> selectedname = teachers[12]
+                    13 -> selectedname = teachers[13]
+                    14 -> selectedname = teachers[14]
+                    15 -> selectedname = teachers[15]
+                    16 -> selectedname = teachers[16]
+                    17 -> selectedname = teachers[17]
+                    18 -> selectedname = teachers[18]
+                    19 -> selectedname = teachers[19]
+                    20 -> selectedname = teachers[20]
+                    21 -> selectedname = teachers[21]
+                    22 -> selectedname = teachers[22]
+                    23 -> selectedname = teachers[23]
+                    24 -> selectedname = teachers[24]
+                    25 -> selectedname = teachers[25]
+                    26 -> selectedname = teachers[26]
+                    27 -> selectedname = teachers[27]
+                    28 -> selectedname = teachers[28]
+                    29 -> selectedname = teachers[29]
+                    30 -> selectedname = teachers[30]
+                    31 -> selectedname = teachers[31]
+                    32 -> selectedname = teachers[32]
+                    33 -> selectedname = teachers[33]
+                    34 -> selectedname = teachers[34]
+                    35 -> selectedname = teachers[35]
+                    36 -> selectedname = teachers[36]
+                    37 -> selectedname = teachers[37]
+                    38 -> selectedname = teachers[38]
+                    39 -> selectedname = teachers[39]
+                    40 -> selectedname = teachers[40]
+                    41 -> selectedname = teachers[41]
+                    42 -> selectedname = teachers[42]
+                    43 -> selectedname = teachers[43]
+                    44 -> selectedname = teachers[44]
+                    45 -> selectedname = teachers[45]
+                    46 -> selectedname = teachers[46]
+                    47 -> selectedname = teachers[47]
                 }
             }
 
@@ -50,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                 .response { request, response, result ->
                     println(request)
                     println(response)
-                    val (bytes, error) = result
+                    val (bytes, _) = result
                     if (bytes != null) {
                         println("[response bytes] ${String(bytes)}")
                     }
